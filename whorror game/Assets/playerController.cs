c907ccb53f9da7e45e4ddf488872f54845a5ced7
Icey_Carp <Dixenbv@gmail.com>
2025-10-09 09:46:04 +0200

    Player controller
    Very basic 2d topdown controller

---
+++ whorror game/Assets/playerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Variables and Components
+    [Header("Variables")]
+    public float _playerMoveSpeed;
+
+    [Header("Components")]
+    [SerializeField] private Rigidbody2D _playerRB;
+    #endregion
+
+
+    void Update()
+    {
+        float _speedX, _speedY;
+        _speedX = Input.GetAxisRaw("Horizontal");
+        _speedY = Input.GetAxisRaw("Vertical");
+
+        _playerRB.linearVelocity = new Vector2(_speedX, _speedY).normalized * _playerMoveSpeed; 
+    }
+}
